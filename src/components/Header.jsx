026eb39b51cb78770615734ef6c8cce240941f3b
irati88 <irati.a88@gmail.com>
2025-10-21 21:28:24 +0200

    Header Html & Css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,43 @@
-//componentes estáticos
-import React from 'react';
 import "../styles/header.css";
+import logo2 from "../images/logo_fondo.png";
+import menu from "../images/menu_icon.svg";
 
 const Header = () => {
   return (
-<>
-</>
-    );
+    <>
+      <header className="header">
+        <div className="header-top">
+          <div className="menu">
+            <img
+              src={menu}
+              alt="Menu"
+              className="menu-icon"
+              //onclick={toggleMenu}
+            />
+
+            <ul className="menu-list">
+              <li>
+                <a href="#">Menú principal</a>
+              </li>
+              <li>
+                <a href="#">Nuestros proyectos</a>
+              </li>
+              <li>
+                <a href="#">Vista previa</a>
+              </li>
+            </ul>
+          </div>
+          {/* <h2 className="company-name">ProyectHadas</h2> */}
+          <img src={logo2} alt="Logo ProyectHadas" className="header-logo" />
+        </div>
+
+        <div className="header-text">
+          <h1>ProyectHadas</h1>
+          <h3>Proyectos que cuentan historias.</h3>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
--- src/pages/AddProjectPage.jsx
+++ src/pages/AddProjectPage.jsx
@@ -1,18 +1,10 @@
+import Header from "../components/Header";
+import "../styles/header.css";
+
 const AddProjectPage = () => {
   return (
     <>
-      {/* Menú hamburguesa con link a otras secciones */}
-      
-      <ul>
-        <li>Lista de proyectos</li>
-      </ul>
-      <img src="preview tarjeta" alt="" />
-      <form>
-        {/* Formulario para crear nuevo proyecto con 10 inputs */}
-        <button>Subir foto autora</button>
-        <button>Subir foto proyecto</button>
-        <button>Subir proyecto</button>
-      </form>
+      <Header />
     </>
   );
 };
