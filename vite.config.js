1a9f4d5642b2a14088602ebd1552e0bf830c60f3
karengonsan <karenglezschez@gmail.com>
2025-10-29 21:22:14 +0100

    deploy de su pm

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/repaso-final-promo-55/",
   plugins: [react()],
   server: {
     open: "/",
