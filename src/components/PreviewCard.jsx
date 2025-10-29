f5454c1db573302e39422b76c57778f9f3a1895c
Karengonsan <karenglezschez@gmail.com>
2025-10-29 10:38:25 +0100

    Retoquitos varios de identación, orden de imports, tecnologies por technologies, borrado de cositas dobles...

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -1,68 +1,63 @@
-// muestra los datos introducidos en el formulario
+import "react";
+import Avatar from "../images/avatar.webp";
+import GitHubIcon from "../images/github.svg";
+import DemoIcon from "../images/globe-solid.svg";
+import PropTypes from "prop-types";
+import "../styles/previewCard.css";
 
-import 'react'
-import PropTypes from 'prop-types'
-import Avatar from '../images/avatar.webp'
-import GitHubIcon from '../images/github.svg'
-import DemoIcon from '../images/globe-solid.svg'
-import '../styles/previewCard.css'
+const PreviewCard = ({ formData, authorImage }) => {
+  const { name, slogan, technologies, repo, demo, desc, autor, job } =
+    formData || {};
 
-
-const PreviewCard = ({formData, authorImage}) =>{
-    const {name, slogan, tecnologies, repo, demo, desc, autor, job} = formData || {};
-
-    return (
-        <>
-        <div className='preview-container'>
-            <div className='preview-img'>
-                <div className='image'>
-                    <img 
-                        src={authorImage? authorImage : Avatar} 
-                        alt={autor ||"Nombre del autora"}   
-                        className="autor-image" 
-                    />
-                </div>
-                <div className='autor-contact'>
-                    <p>{job || "Developer"}</p>
-                    <h4>{autor || "Cármen Sánchez"}</h4>
-                        <div className='contact'>
-                            <p>{tecnologies || "HTML |CSS| REACT"}</p>
-                            <p className="contact-item">
-                                <img src={GitHubIcon} alt="Repo" className="icon" />
-                                {repo || "github.com/ejemplo"}
-                            </p>
-                            <p className="contact-item">
-                                <img src={DemoIcon} alt="Demo" className="icon" />
-                                {demo || "www.ejemplo.com"}
-                            </p>
-                        </div>    
-                </div>
+  return (
+    <>
+      <div className="preview-container">
+        <div className="preview-img">
+          <div className="image">
+            <img
+              src={authorImage ? authorImage : Avatar}
+              alt={autor || "Nombre del autora"}
+              className="autor-image"
+            />
+          </div>
+          <div className="autor-contact">
+            <p>{job || "Developer"}</p>
+            <h4>{autor || "Cármen Sánchez"}</h4>
+            <div className="contact">
+              <p>{technologies || "HTML |CSS| REACT"}</p>
+              <p className="contact-item">
+                <img src={GitHubIcon} alt="Repo" className="icon" />
+                {repo || "github.com/ejemplo"}
+              </p>
+              <p className="contact-item">
+                <img src={DemoIcon} alt="Demo" className="icon" />
+                {demo || "www.ejemplo.com"}
+              </p>
             </div>
-            <div className='preview-data'>
-                <h2>{name || "Proyecto Hadas"}</h2>
-                <p>{slogan || "ProjectHadas"}</p>
-                <p className='desc'>{desc || "Aquí puedes decribir brevemente tu proyecto."}</p>
-            </div>             
+          </div>
         </div>
-        </>
-    )
-}
-
-export default PreviewCard
+        <div className="preview-data">
+          <h2>{name || "Proyecto Hadas"}</h2>
+          <p>{slogan || "ProjectHadas"}</p>
+          <p className="desc">
+            {desc || "Aquí puedes decribir brevemente tu proyecto."}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 PreviewCard.propTypes = {
   name: PropTypes.string,
   slogan: PropTypes.string,
-  tecnologies: PropTypes.string,
+  technologies: PropTypes.string,
   repo: PropTypes.string,
   demo: PropTypes.string,
   desc: PropTypes.string,
   image: PropTypes.string,
   autor: PropTypes.string,
-  job: PropTypes.string
+  job: PropTypes.string,
 };
 
-
-
-
-
+export default PreviewCard;
