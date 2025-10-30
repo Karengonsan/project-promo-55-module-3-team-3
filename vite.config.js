299fab799c90f7d04be66467a1b17baf129f9fd3
karengonsan <karenglezschez@gmail.com>
2025-10-30 09:55:32 +0100

    publicacion

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/repaso-final-promo-55/",
   plugins: [react()],
+  base: "/project-promo-55-module-3-team-3/",
   server: {
     open: "/",
     watch: {
-      usePolling: true
-    }
-  }
+      usePolling: true,
+    },
+  },
 });
-
